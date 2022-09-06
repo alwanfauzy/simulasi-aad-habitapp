57bc9523cdf41fd474b8ec8298e7dfafbf6b8ea0
alwanfauzy <alwanfauzi13@gmail.com>
2022-09-06 23:11:00 +0700

    Solve TODO 14

--- app/src/main/java/com/dicoding/habitapp/ui/random/RandomHabitAdapter.kt
+++ app/src/main/java/com/dicoding/habitapp/ui/random/RandomHabitAdapter.kt
@@ -3,6 +3,9 @@ package com.dicoding.habitapp.ui.random
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.dicoding.habitapp.R
 import com.dicoding.habitapp.data.Habit
@@ -19,8 +22,10 @@ class RandomHabitAdapter(
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-        PagerViewHolder(LayoutInflater.from(parent.context)
-            .inflate(R.layout.pager_item, parent, false))
+        PagerViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.pager_item, parent, false)
+        )
 
     override fun onBindViewHolder(holder: PagerViewHolder, position: Int) {
         val key = getIndexKey(position) ?: return
@@ -41,7 +46,20 @@ class RandomHabitAdapter(
         //TODO 14 : Create view and bind data to item view
 
         fun bind(pageType: PageType, pageData: Habit) {
-
+            itemView.findViewById<TextView>(R.id.pager_tv_title).text = pageData.title
+            itemView.findViewById<TextView>(R.id.pager_tv_start_time).text = pageData.startTime
+            itemView.findViewById<ImageView>(R.id.item_priority_level)
+                .setImageResource(
+                    when (pageType) {
+                        PageType.HIGH -> R.drawable.ic_priority_high
+                        PageType.MEDIUM -> R.drawable.ic_priority_medium
+                        PageType.LOW -> R.drawable.ic_priority_low
+                    }
+                )
+            itemView.findViewById<TextView>(R.id.pager_tv_minutes).text =
+                pageData.minutesFocus.toString()
+            itemView.findViewById<Button>(R.id.pager_btn_open_count_down)
+                .setOnClickListener { onClick(pageData) }
         }
     }
 }
