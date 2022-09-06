83138d69b36fb4602ff95a37236c7ded6bf50580
alwanfauzy <alwanfauzi13@gmail.com>
2022-09-06 23:15:52 +0700

    Solve TODO 16

--- app/src/androidTest/java/com/dicoding/habitapp/ui/list/HabitActivityTest.kt
+++ app/src/androidTest/java/com/dicoding/habitapp/ui/list/HabitActivityTest.kt
@@ -1,6 +1,40 @@
 package com.dicoding.habitapp.ui.list
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import com.dicoding.habitapp.R
+import com.dicoding.habitapp.ui.add.AddHabitActivity
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
 //TODO 16 : Write UI test to validate when user tap Add Habit (+), the AddHabitActivity displayed
 class HabitActivityTest {
+    @get:Rule
+    val activityRule = ActivityScenarioRule(HabitListActivity::class.java)
 
+    @Before
+    @Throws(Exception::class)
+    fun setUp() {
+        Intents.init()
+    }
+
+    @After
+    @Throws(Exception::class)
+    fun tearDown() {
+        Intents.release()
+    }
+
+    @Test
+    fun whenClickAddHabit_ShouldDisplayAddHabitActivity() {
+        onView(withId(R.id.fab)).perform(click())
+
+        intended(hasComponent(AddHabitActivity::class.java.name))
+    }
 }
