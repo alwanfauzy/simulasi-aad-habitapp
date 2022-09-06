5c95283a0f605c12fba9201227b82043959ca194
alwanfauzy <alwanfauzi13@gmail.com>
2022-09-06 23:07:41 +0700

    Solve TODO 10 & 13

--- app/src/main/java/com/dicoding/habitapp/ui/countdown/CountDownActivity.kt
+++ app/src/main/java/com/dicoding/habitapp/ui/countdown/CountDownActivity.kt
@@ -1,13 +1,20 @@
 package com.dicoding.habitapp.ui.countdown
 
+import android.app.NotificationChannel
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.work.Data
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
 import com.dicoding.habitapp.R
 import com.dicoding.habitapp.data.Habit
+import com.dicoding.habitapp.notification.NotificationWorker
 import com.dicoding.habitapp.utils.HABIT
+import com.dicoding.habitapp.utils.HABIT_ID
+import com.dicoding.habitapp.utils.HABIT_TITLE
 
 class CountDownActivity : AppCompatActivity() {
 
@@ -23,15 +30,35 @@ class CountDownActivity : AppCompatActivity() {
         val viewModel = ViewModelProvider(this).get(CountDownViewModel::class.java)
 
         //TODO 10 : Set initial time and observe current time. Update button state when countdown is finished
+        viewModel.setInitialTime(habit.minutesFocus)
+        val tvCurrentTime = findViewById<TextView>(R.id.tv_count_down)
+        viewModel.currentTimeString.observe(this) {
+            tvCurrentTime.text = it
+        }
 
         //TODO 13 : Start and cancel One Time Request WorkManager to notify when time is up.
+        viewModel.eventCountDownFinish.observe(this) {
+            updateButtonState(!it)
+
+            val builder = OneTimeWorkRequestBuilder<NotificationWorker>().apply {
+                setInputData(
+                    Data.Builder()
+                        .putInt(HABIT_ID, habit.id)
+                        .putString(HABIT_TITLE, habit.title)
+                        .build()
+                )
+            }.build()
+            WorkManager.getInstance(this).enqueue(builder)
+        }
 
         findViewById<Button>(R.id.btn_start).setOnClickListener {
-
+            viewModel.startTimer()
+            updateButtonState(true)
         }
 
         findViewById<Button>(R.id.btn_stop).setOnClickListener {
-
+            viewModel.resetTimer()
+            updateButtonState(false)
         }
     }
 
